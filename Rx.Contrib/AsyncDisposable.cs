9df778f89df5b9b32f8e2d714a397488a6b96484
romerod <daniel.romero@baseclass.ch>
2017-07-13 09:54:54 +0200

    Switch to .net standard 1.5 and support for cancelable subscriptions

---
+++ Rx.Contrib/AsyncDisposable.cs
@@ -0,0 +1,65 @@
+﻿namespace Rx.Contrib
+{
+    using System;
+    using System.Threading;
+    using System.Threading.Tasks;
+
+    /// <summary>
+    ///     Represents an asynchronous Func{Task}-based disposable and contains helpers methods to work with <see cref="IAsyncDisposable"/>.
+    /// </summary>
+    public class AsyncDisposable : IAsyncDisposable
+    {
+        #region Fields
+
+        private Func<Task> disposeTask;
+
+        #endregion
+
+        #region Constructors and Destructors
+
+        private AsyncDisposable(
+            Func<Task> disposeTask)
+        {
+            this.disposeTask = disposeTask;
+        }
+
+        #endregion
+
+        #region Public Methods and Operators
+
+        /// <summary>
+        ///     Creates a asynchronous disposable object that invokes the specified operation when disposed.
+        /// </summary>
+        /// <param name="disposeAsync">
+        ///     Operation to run during the first call to <see cref="IAsyncDisposable.DisposeAsync"/>. 
+        ///     The operation is guaranteed to be run at most once.
+        /// </param>
+        /// <returns>
+        ///     The disposable object that runs the given function upon disposal.
+        /// </returns>
+        public static IAsyncDisposable Create(
+            Func<Task> disposeAsync)
+        {
+            return new AsyncDisposable(disposeAsync);
+        }
+
+        /// <summary>
+        ///     Calls the disposal function if and only if the current instance hasn't been disposed yet.
+        /// </summary>
+        /// <returns>
+        ///   A <see cref="Task"/> representing the outcome of the operation.
+        /// </returns>
+        public Task DisposeAsync()
+        {
+            var localDisposeTask = Interlocked.Exchange(ref this.disposeTask, null);
+
+            var task = localDisposeTask == null
+                           ? Task.CompletedTask
+                           : localDisposeTask();
+
+            return task;
+        }
+
+        #endregion
+    }
+}
---
+++ Rx.Contrib/ObserverWithCancellationSupport.cs
@@ -0,0 +1,78 @@
+﻿namespace Rx.Contrib
+{
+    using System;
+    using System.Threading;
+    using System.Threading.Tasks;
+
+    /// <summary>
+    ///     Observer which holds a task completion source which can be used to request observable completion.
+    /// </summary>
+    /// <typeparam name="TSource">
+    ///     The source type.
+    /// </typeparam>
+    internal sealed class ObserverWithCancellationSupport<TSource> : IObserver<TSource>
+    {
+        #region Constructors and Destructors
+
+        public ObserverWithCancellationSupport(
+            CancellationToken token,
+            TaskCompletionSource<object> taskCompletionSource,
+            Action<TSource> onNext = null,
+            Action<Exception> onError = null,
+            Action onCompleted = null)
+        {
+            this.Token = token;
+            this.taskCompletionSource = taskCompletionSource;
+
+            this.onNext = onNext ?? (o => { });
+            this.onError = onError ?? (ex => { });
+            this.onCompleted = onCompleted ?? (() => { });
+        }
+
+        #endregion
+
+        #region Fields
+
+        private readonly Action onCompleted;
+
+        private readonly Action<Exception> onError;
+
+        private readonly Action<TSource> onNext;
+
+        private readonly TaskCompletionSource<object> taskCompletionSource;
+
+        #endregion
+
+        #region Public Properties
+
+        public bool IsCompleted { get; set; }
+
+        public CancellationToken Token { get; }
+
+        #endregion
+
+        #region Public Methods and Operators
+
+        public void OnCompleted()
+        {
+            this.IsCompleted = true;
+            this.taskCompletionSource.SetResult(0);
+            this.onCompleted();
+        }
+
+        public void OnError(
+            Exception error)
+        {
+            this.taskCompletionSource.SetException(error);
+            this.onError(error);
+        }
+
+        public void OnNext(
+            TSource value)
+        {
+            this.onNext(value);
+        }
+
+        #endregion
+    }
+}
