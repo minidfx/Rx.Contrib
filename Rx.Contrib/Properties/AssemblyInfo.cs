8f66b12ec3d45e45c449cf111281d3bb590de6b1
Benjamin Burgy <benjamin@setza.com>
2018-04-19 07:15:25 +0200

    Fix build on Travis.

--- Rx.Contrib/Properties/AssemblyInfo.cs
+++ Rx.Contrib/Properties/AssemblyInfo.cs
@@ -14,7 +14,6 @@ using System.Runtime.InteropServices;
 // to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
 
-[assembly: CLSCompliant(true)]
 [assembly: ComVisible(false)]
 
 // Version information for an assembly consists of the following four values:
